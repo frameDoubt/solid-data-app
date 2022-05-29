009b65cf3eecae0991f05ca854c875d72db76bff
Brandon Wright <brandon.w.wright4@gmail.com>
2022-05-29 16:33:47 -0700

    add style class input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,14 @@
 import { createSignal } from 'solid-js';
+import styles from '../App.module.css';
 
 function Input() {
   const [subject, setSubject] = createSignal("");
-
+  
   return (
+    
     <label>
       <input
+        class={styles.input}
         type="text"
         id="subject"
         value={subject()}
